1e57bffdc897d1ed2207cd76e176923ebdc59db2
arkoaikins <arkoaikins@gmail.com>
2024-01-20 19:40:26 -0500

    write how to create arrays in python

---
+++ concepts/array/create_array.py
@@ -0,0 +1,8 @@
+# in python you need to import the array module
+# to be able to use arrays
+
+import array
+
+balance = array.array('i', [300,200,100])
+print(balance)
+# Write comment on how this works
